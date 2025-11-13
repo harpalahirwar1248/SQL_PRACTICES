9e4f9a12a92c1eb5d9bfc7149b10f8989e3478be
harpalahirwar1248 <vijayvarma7969@gmail.com>
2025-11-13 16:25:32 +0530

    Add files via upload

--- SQL PRACTICES.sql
+++ SQL PRACTICES.sql
@@ -160,3 +160,182 @@ insert into company_1(emp_id,emp_name,emp_age,salary)values (5,'manish',26,76000
 select * from company_1;
 select distinct emp_id,emp_name
 from company_1;
+
+create table person(
+per_id int,
+per_name varchar(40),
+per_surename varchar(40),
+per_age int
+);
+insert into person(per_id,per_name,per_surename,per_age)values 
+(1,'manish','yadav',26),
+(2,'raj','varma',21),
+(3,'mahendra','dhoni',24),
+(4,'virat','kohali',28),
+(5,'satish','yadav',20),
+(6,'anuj','yadav',17),
+(7,'pintoo','kevat',16),
+(8,'deep','kumar',18),
+(9,'harpal','ahirwar',25),
+(10,'pavan','singh',29);
+
+select * from person;
+
+create table orders(
+ord_id int,
+ord_num int,
+per_id int
+);
+insert into orders(ord_id,ord_num,per_id)values
+(101,1001,1), 
+(102,1002,2), 
+(103,1003,3), 
+(104,1004,4), 
+(105,1005,5), 
+(106,1006,6), 
+(107,1007,7), 
+(108,1008,8), 
+(109,1009,9),
+(110,1010,10);
+select * from orders;
+select distinct per_id,per_id
+from orders;
+drop table person;
+create table person(
+per_id int,
+per_name varchar(40), 
+per_surename varchar(40),
+per_age int
+);
+insert into person(per_id,per_name,per_surename,per_age)values 
+(1,'per_name','yadav',26),
+(2,'per_name','varma',21),
+(3,'per_name','dhoni',24),
+(4,'per_name','kohali',28),
+(5,'per_name','yadav',20),
+(6,'per_name','yadav',17),
+(7,'per_name','kevat',16),
+(8,'per_name','kumar',18),
+(9,'per_name','ahirwar',25),
+(10,'per_name','singh',29);
+
+select * from person;
+drop table person;
+create table person(
+per_id int not null,
+per_name varchar(40) unique,
+per_surename varchar(40) unique,
+per_age int unique
+);
+insert into person(per_id,per_name,per_surename,per_age)values 
+(1,'manish','yadav',26),
+(2,'raj','varma',21),
+(3,'mahendra','dhoni',24),
+(4,'virat','kohali',18),
+(5,'satish','yadav',20),
+(6,'anuj','yadav',16),
+(7,'pintoo','kevat',16),
+(8,'raj','kumar',18),
+(9,'harpal','ahirwar',25),
+(10,'pavan','singh',29);
+drop table person;
+select * from person;
+create table person(
+per_id int not null,
+per_name varchar(40) Not null  unique,
+per_surename varchar(40) unique,
+per_age int unique
+);
+drop table orders;
+create table orders(
+ord_id int not null,
+ord_num int unique,
+per_id int not null 
+);
+insert into orders(ord_id,ord_num,per_id)values
+(101,1001,1), 
+(102,1002,2), 
+(103,1003,3), 
+(104,1004,4), 
+(105,1005,5);
+select * into orders;
+create table person(
+per_id int not null,
+per_name varchar(40) null,
+per_surename varchar(40) null,
+per_age int unique
+);
+insert into person(per_id,per_name,per_surename,per_age)values 
+(1,'manish','yadav',26),
+(2,'raj','varma',21),
+(3,'mahendra','dhoni',24),
+(4,'virat','kohali',18),
+(5,'satish','yadav',20),
+(6,'anuj','yadav',16);
+insert into person(per_id,per_name,per_surename,per_age)values 
+(1,'','',17),
+(5,'','',14),
+(1,'','',9);
+
+select * from person; 
+
+DELETE FROM person
+WHERE per_name = null;
+
+drop database company;
+
+
+CREATE DATABASE company_db;
+USE company_db;
+CREATE TABLE company (
+    com_id INT PRIMARY KEY AUTO_INCREMENT,
+    com_name VARCHAR(50),
+    city VARCHAR(50),
+    state VARCHAR(50)
+);
+
+INSERT INTO company(com_id,com_name, city, state)VALUES 
+(1,'TechSol Pvt Ltd', 'Bhopal', 'Madhya Pradesh'),
+(2,'DataVision Ltd', 'Indore', 'Madhya Pradesh');
+ select * from company;     
+ 
+ CREATE TABLE empl (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50),
+    salary DECIMAL(10,2) CHECK (salary >= 10000),
+    age INT CHECK (age > 25)
+);
+INSERT INTO empl (id,name, salary, age) 
+VALUES (1,'Rohan', 15000, 28),
+       (2,'Meena', 20000, 30);
+select * from empl;  
+ALTER TABLE empl
+ADD COLUMN city VARCHAR(30);
+
+UPDATE empl
+SET city = 'Bhopal'
+WHERE id = 1;
+update empl
+SET city = 'indore'
+WHERE id = 2;
+
+select *
+FROM empl
+WHERE salary >= 15000;
+
+     
+
+      
+       
+
+
+
+
+
+
+
+
+
+
+
+
