e6abc74d03a68e58b3ef8c641aef2483f5ac75a3
harpalahirwar1248 <vijayvarma7969@gmail.com>
2025-11-19 21:38:55 +0530

    Add files via upload

--- learning sql.sql
+++ learning sql.sql
@@ -18,4 +18,28 @@ alter table demo18
 ADD Birthday DATE;
 insert into demo18(name,created_date,Birthday)values('shiva',11/02/2026,14/08/2005);
 select * from demo18;
+create table student1(
+stu_id int auto_increment primary key check(stu_id>11),
+stu_name varchar(10),
+age int check(age>=18)
+);
+insert into student1(stu_id,stu_name,age)values
+(1,'rohit',22),
+(2,'raj',25);
+select * from student1;
+insert into student1(stu_name, age) values
+('rohit', 22), 
+('raj', 25);
+select* from student1;
+truncate table student1;
+ALTER TABLE student1 
+MODIFY stu_id bigint,
+modify stu_name char(8),
+modify age float; 
+describe student1;
+
+
+
+
+
 
